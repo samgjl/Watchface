b5d53795cb18b5ed1b5afa1df1702c396d0458e9
samgjl <johnsonlacosss@carleton.edu>
2023-08-01 23:29:09 -0700

    oops pt. 2...

--- watchface.py
+++ watchface.py
@@ -70,7 +70,8 @@ class WatchWidget(RelativeLayout):
         with self.canvas.before:
             Rectangle(source='media/bkgd_horiz.png', size=Window.size)
         
-        layout = RelativeLayout(size_hint=(.75, .75), pos_hint={'x':0.125, 'y':0.15})
+        # layout = RelativeLayout(size_hint=(.75, .75), pos_hint={'x':0.125, 'y':0.15})
+        layout = RelativeLayout(size=Window.size)
         self.add_widget(layout)
 
         project_bootleg = Image(source='media/project_bootleg.png', 
