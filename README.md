775f5c041a76da3ff733379beb0240b4674ff002
samgjl <johnsonlacosss@carleton.edu>
2023-08-01 21:48:30 -0700

    Final Product?!?!?!?!

--- README.md
+++
@@ -1,5 +0,0 @@
-# Watchface
-Hopefully this will build out to be a fun exploration of the Raspberry Pi and Hobie Brown's watch.
-
-# Activate virtual environment:
-venv\Scripts\activate
--- watchface.py
+++ watchface.py
@@ -21,7 +21,7 @@ Config.set('graphics', 'height', '320')
 # Config.set('graphics', 'resizable', False)
 Config.write()
 Window.size = (480, 320)
-LabelBase.register(name='Glitchy', fn_regular='GlitchGoblin.ttf') # Font
+LabelBase.register(name='Glitchy', fn_regular='fonts/GlitchGoblin.ttf') # Font
 
 # Helper Functions:
 def get_date():
@@ -59,10 +59,8 @@ class QuitButton(Image):
 class PowerButton(Image):
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
-        #    os.system("sudo halt\n")
+           os.system("sudo halt\n")
            quit()
-
-
 
 class WatchWidget(RelativeLayout):
     def __init__(self, **args):
@@ -70,49 +68,68 @@ class WatchWidget(RelativeLayout):
     def __init__(self, **kwargs):
         super(RelativeLayout, self).__init__(**kwargs)
         with self.canvas.before:
-            Rectangle(source='bkgd_test.png', size=Window.size)
+            Rectangle(source='media/bkgd_horiz.png', size=Window.size)
         
-        layout = RelativeLayout(size=Window.size)
+        layout = RelativeLayout(size_hint=(.65, .65), pos_hint={'x':0.165, 'y':0.165})
         self.add_widget(layout)
 
-        project_bootleg = Image(source='project_bootleg.png', 
+        project_bootleg = Image(source='media/project_bootleg.png', 
                                 fit_mode="scale-down", 
                                 pos_hint={'x':0.075, 'y':0.375},
+                                center_x=0.5,
+                                center_y=0.5,
                                 size_hint_x=0.85,
                                 size_hint_y=0.85)
         layout.add_widget(project_bootleg)
 
-        flag = Image(source='IMG_6934.png', 
-                     pos_hint={'x':0, 'y':0.1},)
+        flag = Image(source='media/uk_smile.png', 
+                     pos_hint={'x':0.4, 'y':0.475},
+                     size_hint_x=0.2,
+                     size_hint_y=0.2,
+                     center_x=0.5,
+                     center_y=0.5)
         layout.add_widget(flag)
 
 
-        clock = ClockWidget(size_hint_x=1, size_hint_y=1, font_size=30,
+        clock = ClockWidget(size_hint_x=1, size_hint_y=1, font_size=24,
                             font_name="Glitchy", 
                             color=(21/255, 230/255, 250/255, 1),
-                            pos_hint={'x':0.1275, 'y':-0.1}, )
+                            pos_hint={'x':0.1275, 'y':-0.155}, )
         Clock.schedule_interval(clock.update, 1/5)
         layout.add_widget(clock)
 
-        destination = Image(source="destination.png", 
+        oval = Image(source="media/oval.png",
+                     allow_stretch=True,
+                     keep_ratio=False,
+                     size_hint_x=0.9,
+                     size_hint_y=0.66,
+                     pos_hint={'x':0.04, 'y':0.0},
+                     center=(0.5, 0.5))
+        layout.add_widget(oval)
+        
+        destination = Image(source="media/destination.png", 
                             fit_mode="scale-down",
                             size_hint_x=0.4,
                             size_hint_y=0.4,
-                            pos_hint={'x':0.1, 'y':0.2}
+                            pos_hint={'x':0.1, 'y':0.15}
                             )
         layout.add_widget(destination)
         
-        bottom_smiley = Label(text='":smile:"', pos_hint={'x':0.0, 'y':-0.25})
+        bottom_smiley = Image(source="media/smile.png",
+                              pos_hint={'x':0.45, 'y':-0.35},
+                              size_hint_x=0.1,
+                              )
         layout.add_widget(bottom_smiley)
+        quote_1 = Image(source="media/open_quote.png",
+                        pos_hint={'x':0.4125, 'y':-0.325},
+                        size_hint_x=0.035)
+        quote_2 = Image(source="media/close_quote.png",
+                        pos_hint={'x':0.55, 'y':-0.325},
+                        size_hint_x=0.035)
+        layout.add_widget(quote_1)
+        layout.add_widget(quote_2)
 
-        # quitbutton = QuitButton(source="quitbutton.png", 
-        #                           pos_hint={'x':0.925, 'y':0.9},
-        #                           size_hint_x=0.1,
-        #                           size_hint_y=0.1,
-        #                           )
-        # layout.add_widget(quitbutton)
-
-        powerbutton = QuitButton(source="powerbutton.png", 
+        powerbutton = QuitButton(source="media/powerbutton.png", 
                                   pos_hint={'x':0.9, 'y':0.05},
                                   size_hint_x=0.1,
                                   size_hint_y=0.1,
